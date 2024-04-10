dde02c710d4daaecaa7ddcb9065a1355dac659a0
unknown <kiowesmith@gmail.com>
2024-04-10 15:42:54 -0400

    finish captcha

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,10 +6,12 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import Login from './pages/Login';
+import { UserContextProvider } from './config/UserContext';
 
 function App() {
   return (
     <div className="App">
+      <UserContextProvider>
         <BrowserRouter>
           <Navbar />
           <div className='pages'>
@@ -20,6 +22,7 @@ function App() {
           </div> 
           <Footer />
         </BrowserRouter>
+      </UserContextProvider>
     </div>
   );
 }
